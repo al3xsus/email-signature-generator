25fea377b74d1ab5af9111356c885383942f0d05
Alexandr <al3xsus@pm.me>
2022-06-03 21:27:19 +0600

    temp fix

--- src/Signature.tsx
+++ src/Signature.tsx
@@ -37,16 +37,16 @@ const Signature = (props: PhotoSignatureProps) => {
                 <td colSpan={3}>
                     <div className={"social-logos-frame"}>
                         <a href={"https://twitter.com/"}>
-                            <img src={TwitterLogo} alt={""}/>
+                            <img src={"https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png"} width={"32px"} height={"32px"} alt={""}/>
                         </a>
                         <a href={"https://www.linkedin.com/"}>
-                            <img src={LinkedInLogo} alt={""}/>
+                            <img src={"https://www.freepnglogos.com/uploads/linkedin-social-media-logo-7.png"} width={"32px"} height={"32px"} alt={""}/>
                         </a>
                         <a href={"https://www.facebook.com/"}>
-                            <img src={FacebookLogo} alt={""}/>
+                            <img src={"https://www.freepnglogos.com/uploads/facebook-logo-14.png"} width={"32px"} height={"32px"} alt={""}/>
                         </a>
                         <a href={"https://www.behance.net/"}>
-                            <img src={BehanceLogo} alt={""}/>
+                            <img src={"https://seeklogo.com/images/B/behance-icon-logo-287E5C6D93-seeklogo.com.png"} width={"32px"} height={"32px"} alt={""}/>
                         </a>
                     </div>
                 </td>
